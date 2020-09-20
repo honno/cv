4da0b484ea3793168e2044e0be226f4225d941c7
Matthew Barber <quitesimplymatt@gmail.com>
2020-09-20 13:32:56 +0100

    sources folder for svgs

--- export_logos.sh
+++ export_logos.sh
@@ -1,5 +1,5 @@
-inkscape github.svg -o github.pdf --export-latex
-inkscape pypi.svg -o pypi.pdf --export-latex
-inkscape kaggle.svg -o kaggle.pdf --export-latex
-inkscape site.svg -o site.pdf --export-latex
+inkscape sources/github.svg -o github.pdf --export-latex
+inkscape sources/pypi.svg -o pypi.pdf --export-latex
+inkscape sources/kaggle.svg -o kaggle.pdf --export-latex
+inkscape sources/site.svg -o site.pdf --export-latex
 
