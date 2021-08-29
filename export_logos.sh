9846959b3ada41aa8f218e0a8cf6716421d106e4
Matthew Barber <quitesimplymatt@gmail.com>
2021-08-29 17:04:20 +0100

    aug 2021 update

--- export_logos.sh
+++ export_logos.sh
@@ -2,4 +2,5 @@ inkscape sources/github.svg -o github.pdf --export-latex
 inkscape sources/pypi.svg -o pypi.pdf --export-latex
 inkscape sources/kaggle.svg -o kaggle.pdf --export-latex
 inkscape sources/site.svg -o site.pdf --export-latex
+inkscape sources/linkedin.svg -o linkedin.pdf --export-latex
 
